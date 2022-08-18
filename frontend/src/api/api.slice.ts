5cc60a2cdc98e2adfdfb69dbbebcfefbe48086dd
Alex M <alexmunoz91@gmail.com>
2022-08-18 10:39:30 +0200

    Refactor component: - Add baseurl type - Add PetrolStation type - Add getPetrolStations types

--- frontend/src/api/api.slice.ts
+++ frontend/src/api/api.slice.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { PetrolStation } from '@noister/pages/gasazon/types'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL as string }),
   tagTypes: ['PetrolStations'],
   endpoints: (builder) => ({
-    getPetrolStations: builder.query({
+    getPetrolStations: builder.query<PetrolStation[], []>({
       query: () => '/petrol-stations'
     })
   })
