62a3725ab094772a85f7ddb15d98aa7a41471617
Alex M <alexmunoz91@gmail.com>
2022-08-18 10:37:58 +0200

    Refactor component: - Solve eslint issues - Reorganize order of functions

--- frontend/src/pages/gasazon/components/BottlesCalculator.component.tsx
+++ frontend/src/pages/gasazon/components/BottlesCalculator.component.tsx
@@ -9,29 +9,31 @@ function BottlesCalculator() {
   const [liters, setLiters] = useState<number>()
   const [bottles, setBottles] = useState<BottlesCombination>()
 
+  function calculateCombination(totalLiters: number, bottlesTypes: Array<number>) {
+    const sortedBottles = bottlesTypes.sort((a: number, b: number) => b - a)
+    let currentLiters: number = totalLiters
+    const bottlesCombination = sortedBottles.reduce(
+      (acc: BottlesCombination, curr: number) => {
+        if (!currentLiters || curr > currentLiters) return acc
+        const integerDivision = Math.floor(currentLiters / curr)
+        acc[curr] = integerDivision
+        currentLiters -= integerDivision * curr
+        return acc
+      },
+      {}
+    )
+
+    setBottles(bottlesCombination)
+    dispatch(setBottlesCombination(bottlesCombination))
+  }
+
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setLiters(+event.target.value)
   }
 
   function handleOnSubmit(event: { preventDefault: () => void }) {
     event.preventDefault()
-    calculateCombination(liters!, [1, 3, 5, 15])
-  }
-
-  function calculateCombination(liters: number, bottles: Array<number>) {
-    const sortedBottles = bottles.sort((a: number, b: number) => b - a)
-    let currentLiters: number = liters
-    let integerDivision: number
-    const bottlesCombination = sortedBottles.reduce((acc: any, curr: number) => {
-      if (!currentLiters || curr > currentLiters) return acc
-      integerDivision = Math.floor(currentLiters / curr)
-      acc[curr] = integerDivision
-      currentLiters -= integerDivision * curr
-      return acc
-    }, {})
-
-    setBottles(bottlesCombination)
-    dispatch(setBottlesCombination(bottlesCombination))
+    if (liters) calculateCombination(liters, [1, 3, 5, 15])
   }
 
   return (
@@ -41,17 +43,21 @@ function BottlesCalculator() {
       </h2>
       <form className="flex flex-col my-4 p-8  bg-gray-200 mx-auto rounded-md">
         <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-gray-600">
+          <label
+            htmlFor="liters-input"
+            className="block mb-2 text-sm font-medium text-gray-600"
+          >
             Put the quantity of the liters you want
+            <input
+              id="liters-input"
+              type="number"
+              min="0"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="Example: 30"
+              required
+              onChange={handleInputChange}
+            />
           </label>
-          <input
-            type="number"
-            min="0"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="Example: 30"
-            required
-            onChange={handleInputChange}
-          />
         </div>
 
         <button
@@ -67,7 +73,7 @@ function BottlesCalculator() {
         <div className="flex flex-col my-4 p-8 bg-gray-200 mx-auto rounded-md">
           <h3 className="font-semibold text-gray-700">You need buy:</h3>
           <ul className="flex flex-col py-4 items-center bg-white list-disc rounded-md">
-            {Object.entries(bottles)?.map(([key, value]: any) => (
+            {Object.entries(bottles)?.map(([key, value]: Array<string>) => (
               <li key={key + value} className="mt-2">
                 <span>
                   {value} units of {key} liters
